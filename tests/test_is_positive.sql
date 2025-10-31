d3560d4f3f39b475a7a2f9f36c8888c5eec29910
Chris <chris.mannik@edu.turkuamk.fi>
2025-10-31 23:18:22 +0200

    feat: Custom test which check that there isnt negative values
    Made custom test for score, words_per_minute and accuracy so that they cant be negative values

---
+++ tests/test_is_positive.sql
@@ -0,0 +1,19 @@
+{% test is_value_positive(model, column_name)%}
+
+WITH validated_value AS (
+    SELECT
+    {{ column_name }} AS positive_value
+    FROM {{model}}
+),
+
+validation_error AS (
+    SELECT
+        positive_value
+    FROM validated_value
+    WHERE (positive_value < 0)
+)
+
+SELECT *
+FROM validation_error
+
+{% endtest %}
